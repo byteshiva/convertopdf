64c2423169d16517a0715ed6a6522c12c49e3681
Siva <byteshiva@users.noreply.github.com>
2019-12-31 10:16:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const puppeteer = require('puppeteer');
         args: ['--no-sandbox', '--disable-setuid-sandbox'],
   });
   const page = await browser.newPage();
-  // await page.goto('https://news.ycombinator.com', {waitUntil: 'networkidle2'});
   await page.goto('https://itnext.io/using-service-objects-in-go-d899dc599335', {waitUntil: 'networkidle2'});
   await page.pdf({path: 'output.pdf', format: 'A4'});
 
